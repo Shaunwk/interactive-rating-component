8b380eb1cf61f923399662c3fd57134efcb7f3e8
Shaun Kelly <shaunwilliamkelly@gmail.com>
2023-05-08 23:59:51 -0700

    finished project

--- script.js
+++ script.js
@@ -1,46 +1,4 @@
 
-
-
-// const circles = document.getElementsByClassName("circle");
-// let selectedCircle = null;
-
-// for (let i = 0; i < circles.length; i++) {
-//     const circle = circles[i];
-
-//     circle.addEventListener("click", function () {
-//         // Remove the background color from the previously selected circle, if any
-//         if (selectedCircle !== null) {
-//             selectedCircle.style.backgroundColor = "";
-//         }
-
-//         // Set the background color of the clicked circle to grey
-//         this.style.backgroundColor = "grey";
-
-//         // Update the selectedCircle variable to reference the currently selected circle
-//         selectedCircle = this;
-//     });
-// }
-
-
-
-// //Clicking the button to toggle between states//
-
-// const btn = document.querySelector('.btn');
-// const ratingState = document.querySelector('.rating');
-// const thankYouState = document.querySelector('.results');
-
-// function toggleBtn() {
-//     if (thankYouState.style.display === "none") {
-//         thankYouState.style.display = "block";
-//         ratingState.style.display = "none";
-//     } else {
-//         thankYouState.style.display = "none";
-//     }
-// };
-
-// btn.addEventListener('click', toggleBtn);
-
-
 // Get all elements with the class "circle"
 const circles = document.getElementsByClassName("circle");
 
@@ -89,4 +47,4 @@ submitButton.addEventListener("click", function() {
   }
 });
 
-
+// this was all with a lot of help from chat GPT//
